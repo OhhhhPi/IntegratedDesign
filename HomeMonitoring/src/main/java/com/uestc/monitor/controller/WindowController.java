85f645945d3ba3ca441361f28f0329abade10221
OhhhhPi <2298185568@qq.com>
2021-12-15 00:15:00 +0800

    add abnormal situation handling

--- HomeMonitoring/src/main/java/com/uestc/monitor/controller/WindowController.java
+++ HomeMonitoring/src/main/java/com/uestc/monitor/controller/WindowController.java
@@ -1,7 +1,9 @@
 package com.uestc.monitor.controller;
 
 import com.uestc.monitor.domain.model.ResponseModel;
+import com.uestc.monitor.domain.pojo.AbnormalRecord;
 import com.uestc.monitor.domain.pojo.WindowRecord;
+import com.uestc.monitor.service.AbnormalServiceImpl;
 import com.uestc.monitor.service.WindowServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,16 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WindowController {
     private final WindowServiceImpl windowService;
+    private final AbnormalServiceImpl abnormalService;
 
     @Autowired
-    public WindowController(WindowServiceImpl windowService) {
+    public WindowController(WindowServiceImpl windowService, AbnormalServiceImpl abnormalService) {
         this.windowService = windowService;
+        this.abnormalService = abnormalService;
     }
 
     @PostMapping("getWindow")
     public @ResponseBody ResponseModel getWindow(@RequestParam Integer userID,@RequestParam boolean Overlap){
         WindowRecord windowRecord = new WindowRecord().setUserid(userID).setOverlap(Overlap);
         windowService.insert(windowRecord);
+        if(windowRecord.getOverlap()){
+            abnormalService.insert(new AbnormalRecord().setAbnormalUserID(userID).setAbnormalType("Window").setAbnormalContent("Overlap Detected"));
+        }
         return new ResponseModel().setStatus(200).setMsg("ok");
     }
 }
